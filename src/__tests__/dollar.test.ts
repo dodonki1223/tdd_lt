1b0ef5a453254d831bff37ed6969581993dbb562
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2021-12-07 08:47:17 +0900

    :bug: 【第2章 明白な実装】Dollar の副作用の確認
    最初の times 呼び出しで、5ドルはもう5ドルではなく10ドルになってしまうため、再度 times メソッドを呼ぶと 10 * 3 になってしまい、結果として 30ドルになってしまう

--- src/__tests__/dollar.test.ts
+++ src/__tests__/dollar.test.ts
@@ -1,14 +1,9 @@
 import { Dollar } from '../dollar';
 
 test('times', () => {
-  /* 
-    「$5 * 2 = $10」のテスト
-      現状では以下の問題点あり
-        ・フィールドは public
-        ・コードには副作用がある
-        ・金額なのに int型を使っている
-   */
   const five = new Dollar(5);
   five.times(2)
   expect(five.amount).toBe(10);
+  five.times(3)
+  expect(five.amount).toBe(15);
 });
