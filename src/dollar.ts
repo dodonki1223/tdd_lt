c66dd4617cfa852c037586250c294cb9286c6977
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2021-12-07 08:51:28 +0900

    :recycle: 【第2章 明白な実装】Dollarの副作用の解消をする①
    コンストラクタで渡されたものを別の変数に入れて対応する

--- src/dollar.ts
+++ src/dollar.ts
@@ -1,9 +1,12 @@
 export class Dollar {
+  private tmpAmount: number
+
   constructor(public amount: number) {
+    this.tmpAmount = amount
     this.amount = amount
   }
 
   times(multiplier: number) {
-    this.amount *= multiplier
+    this.amount = this.tmpAmount * multiplier
   }
 }
