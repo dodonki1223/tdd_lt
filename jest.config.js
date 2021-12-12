7bf9b110d1d759e56c63635ae2f0509441135c0a
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2021-12-12 18:51:26 +0900

    :wrench: エディタで jest の型がちゃんと認識しないため tsconfig を用意する

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,17 @@
 module.exports = {
-  "roots": [
-    "<rootDir>/src"
-  ],
-  "testMatch": [
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  roots: ["<rootDir>/src"],
+  testMatch: [
     "**/__tests__/**/*.+(ts|tsx|js)",
-    "**/?(*.)+(spec|test).+(ts|tsx|js)"
+    "**/?(*.)+(spec|test).+(ts|tsx|js)",
   ],
-  "transform": {
-    "^.+\\.(ts|tsx)$": "ts-jest"
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
   },
-}
+  globals: {
+    "ts-jest": {
+      tsconfig: "tsconfig.jest.json",
+    },
+  },
+};
