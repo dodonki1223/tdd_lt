a7096e0db3992b90203a67b617b406a667c33ac0
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2021-12-12 19:19:20 +0900

    :recycle: 【第4章 意図を語るテスト】オブジェクトの比較に変更
    ・toBe は ===, toEqual は ==
    ・オブジェクトの比較の場合は == を使う必要があるため、toEqual で行う
    ・同じオブジェクトだが toBe（===） はメモリの参照先まで比較するため使わない

--- src/__tests__/dollar.test.ts
+++ src/__tests__/dollar.test.ts
@@ -3,8 +3,8 @@ import { Dollar } from '../dollar';
 test('times', () => {
   const five = new Dollar(5);
   
-  expect(five.times(2).amount).toBe(10);
-  expect(five.times(3).amount).toBe(15);
+  expect(five.times(2)).toEqual(new Dollar(10));
+  expect(five.times(3)).toEqual(new Dollar(15));
 });
 
 test('equals', () => {
