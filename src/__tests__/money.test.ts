fa24b22d06b8b32409bfdb45b6a297c8cefb52b3
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2021-12-30 14:49:11 +0900

    :sparkles: 【第15章 テスト任せとコンパイラ任せ】 換算処理が抜けていたため追加する

--- src/__tests__/money.test.ts
+++ src/__tests__/money.test.ts
@@ -70,3 +70,12 @@ test('test reduce money different currency', () => {
   const result = bank.reduce(Money.franc(2), 'USD');
   expect(Money.dollar(1)).toEqual(result);
 })
+
+test('test mixed addition', () => {
+  const fiveBucks = Money.dollar(5);
+  const tenFrancs = Money.franc(10);
+  const bank = new Bank();
+  bank.addRate('CHF', 'USD', 2);
+  const result = bank.reduce(fiveBucks.plus(tenFrancs), 'USD');
+  expect(result).toEqual(Money.dollar(10));
+})
--- src/sum.ts
+++ src/sum.ts
@@ -7,7 +7,9 @@ export class Sum implements Expression {
   }
 
   reduce(bank: Bank, to: Currency):Money {
-    const amount :number = this.augend.amount + this.addend.amount;
+    const amount :number =
+      this.augend.reduce(bank, to).amount
+      + this.addend.reduce(bank, to).amount;
     return new Money(amount, to);
   }
 }
